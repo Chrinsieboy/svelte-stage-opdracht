35bac025100beb6541e7db5b229e24998ca58cc7
Chris Friemann <50674927+Chrinsieboy@users.noreply.github.com>
2022-05-21 21:10:07 +0200

    Feat: Get holiday information
    Get holidays of the netherlands and the information of it

--- src/components/HolidayList.svelte
+++ src/components/HolidayList.svelte
@@ -1,18 +1,46 @@
 <script>
     import { onMount } from "svelte";
-    
+
     let holidays = [];
 
     onMount(async () => {
-        const response = await fetch('https://holidays.abstractapi.com/v1/?api_key=8d7d830751a5457bb375cab6ad44aa6e&country=US&year=2020')
-        holidays = await response.json()
-        console.log(holidays)
-    })
+        const response = await fetch(
+            "https://holidays.abstractapi.com/v1/?api_key=8d7d830751a5457bb375cab6ad44aa6e&country=NL&year=2020"
+        );
+        holidays = await response.json();
+        console.log(holidays);
+    });
 </script>
 
 <main>
-    {#each holidays as holiday (holiday)}
-    <p>{holiday.name}</p>
-    <hr>
-    {/each}
+    <div class="holidays">
+        {#each holidays as holiday (holiday)}
+            <div class="holiday">
+                <p>{holiday.name}</p>
+                <ul id="UL">
+                    <li>Holiday Type: {holiday.type}</li>
+                    <li>Holiday date: {holiday.date}</li>
+                    <li>Holiday day of the week: {holiday.week_day}</li>
+                </ul>
+            </div>
+        {/each}
+    </div>
 </main>
+
+<style>
+    .holidays {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: center;
+        align-items: left;
+        text-align: left;
+        /* gap: 1rem; */
+    }
+
+    .holiday {
+        padding: 5px;
+        margin: 10px;
+        border: 2px #ff0000 solid;
+    }
+</style>
